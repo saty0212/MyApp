4c1507a4e9ebbeaf24d1a31668cde0a7f7ecfcb3
Satyendra <sschouhan.cse@mnit.ac.in>
2025-08-30 08:32:38 +0530

    new subtract method added

--- MathUtil.java
+++ MathUtil.java
@@ -1,6 +1,8 @@
 public class MathUtil {
     public static int add(int a, int b) {
-    return a + b;
+        return a + b;
     }
-        
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 }
