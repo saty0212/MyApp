89e02b43c4635d6df2c439c594a2103d110e8dd3
Satyendra <sschouhan.cse@mnit.ac.in>
2025-08-30 08:41:05 +0530

    bug fixed differently by including -1

--- MathUtil.java
+++ MathUtil.java
@@ -1,6 +1,6 @@
 public class MathUtil {
     public static int add(int a, int b) {
-        return a + b;
+        return a + b -1;  //different change
     }
     public static int subtract(int a, int b) {
         return a - b;
