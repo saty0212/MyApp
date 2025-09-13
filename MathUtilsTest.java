01de05a4fa1f06641bd51d6ca450deda88a60777
Satyendra <sschouhan.cse@mnit.ac.in>
2025-09-13 08:53:26 +0530

    modified util test file

--- MathUtilsTest.java
+++ MathUtilsTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MathUtilsTest {
     @Test
     public void testAdd() {
-        assertEquals(6, MathUtil.add(2, 3));
+        assertEquals(5, MathUtil.add(2, 3));
     }
     @Test
     public void testSubtract() {
